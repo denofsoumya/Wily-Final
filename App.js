c5eba8a6779badc57d378af586c4c848acbce9eb
Soumya Xavier <denofsoumya@gmail.com>
2021-03-15 09:00:12 +0530

    commit

--- App.js
+++ App.js
@@ -54,7 +54,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "red",
-    alignItems: "center",
+    alignItems: "center", 
     justifyContent: "center"
   }
 });
